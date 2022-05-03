2e80bd4ede902345a45d422012a58e58af7004b2
oaditya97@gmail.com <oaditya97@gmail.com>
2022-05-03 07:04:10 -0700

    fixed

--- hello/settings.py
+++ hello/settings.py
@@ -116,7 +116,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
 
     BASE_DIR / 'static'
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 #STATICFILES_STORAGE = 'whitenoise.storage..CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/images')
